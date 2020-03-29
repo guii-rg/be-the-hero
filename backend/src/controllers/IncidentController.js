f6c5a4280acb4d17970f975526ae3e06b64dfca6
Guilherme Ribeiro <guii-rg@outlook.com>
2020-03-29 14:25:00 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,4 +1,4 @@
-const connection = require('../database/connection');
+ const connection = require('../database/connection');
 module.exports = {
     async index(request, response){
         const{page = 1} = request.query;
